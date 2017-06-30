a954dc9dc856e1b61f9d57c9ec198b3ceb0a3c8d
Kirtan Thakkar <kirtan403@gmail.com>
2017-06-30 19:10:56 +0530

    addTextWatcher() method for EditTexts with default implementations

---
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/EditText.kt
@@ -0,0 +1,32 @@
+package com.livinglifetechway.k4kotlin
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+
+/**
+ * Accepts 3 text watcher methods with default empty implementation.
+ * Returns the TextWatcher added to EditText
+ */
+fun EditText.addTextWatcher(afterTextChanged: (s: Editable?) -> Unit = { _ -> },
+                            beforeTextChanged: (s: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ -> },
+                            onTextChanged: (s: CharSequence?, start: Int, before: Int, count: Int) -> Unit = { _, _, _, _ -> }): TextWatcher {
+
+    val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            afterTextChanged(s)
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            beforeTextChanged(s, start, count, after)
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            onTextChanged(s, start, before, count)
+        }
+    }
+
+    addTextChangedListener(textWatcher)
+    return textWatcher
+}
