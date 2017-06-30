70ba15af7bb9e18f26da859cc63434f23c86f891
Kirtan Thakkar <kirtan403@gmail.com>
2017-06-30 19:09:57 +0530

    orFalse() method for Boolean? variables

---
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Boolean.kt
@@ -0,0 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.livinglifetechway.k4kotlin
+
+/**
+ * Returns false if it is null
+ */
+fun Boolean?.orFalse(): Boolean? = if (this != null) this else false
