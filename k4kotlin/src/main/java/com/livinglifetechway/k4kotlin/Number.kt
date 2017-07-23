aabb2e16a5906f0b3df7bbb492eb896ffae49ab9
Kirtan Thakkar <kirtan403@gmail.com>
2017-07-23 12:55:47 +0530

    Fixed Boolean return type Added Number options for Int,Float and Double

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Number.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Number.kt
@@ -6,3 +6,9 @@ package com.livinglifetechway.k4kotlin
  * Returns Zero (0) if it is null
  */
 fun Number?.orZero(): Number = if (this != null) this else 0
+
+fun Int?.orZero(): Int = if (this != null) this else 0
+
+fun Float?.orZero(): Float = if (this != null) this else 0.0f
+
+fun Double?.orZero(): Double = if (this != null) this else 0.0
