7303dca883d14932ba8495030dd4a2d3b7589b4a
Kirtan Thakkar <kirtan403@gmail.com>
2017-11-04 14:59:36 +0530

    Added dpToPx and pxToDp converter methods for Context

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Context.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Context.kt
@@ -13,3 +13,13 @@ fun Context.hideViews(vararg views: View) = views.forEach { it.visibility = View
  * Shows all the views passed in the arguments
  */
 fun Context.showViews(vararg views: View) = views.forEach { it.visibility = View.VISIBLE }
+
+/**
+ * Converts px to dp
+ */
+fun Context.pxToDp(px: Float) = px / this.resources.displayMetrics.density
+
+/**
+ * Converts dp to px
+ */
+fun Context.dpToPx(dp: Float) = dp * this.resources.displayMetrics.density
