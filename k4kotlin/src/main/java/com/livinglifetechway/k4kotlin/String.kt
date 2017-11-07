eaa4e8fb595ba71bffba6d033d170094944f8234
Kirtan Thakkar <kirtan403@gmail.com>
2017-11-07 19:24:22 +0530

    String extension methods for valid email and numeric

---
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/String.kt
@@ -0,0 +1,15 @@
+package com.livinglifetechway.k4kotlin
+
+/**
+ * Checks if a string is a valid email
+ */
+fun String.isEmail() = android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+/**
+ * Checks if String is Number.
+ * Checks against regex ^[0-9]+$
+ */
+fun String.isNumeric(): Boolean {
+    val p = "^[0-9]+$".toRegex()
+    return matches(p)
+}
