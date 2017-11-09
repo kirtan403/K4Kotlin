3fb4f0466503d9c720b90ee2f8c8e1d741354cd3
Kirtan Thakkar <kirtan403@gmail.com>
2017-11-09 17:10:36 +0530

    Added getColorCompat and getDrawableCompat for context

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Context.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Context.kt
@@ -1,6 +1,9 @@
 package com.livinglifetechway.k4kotlin
 
 import android.content.Context
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.v4.content.ContextCompat
 import android.view.View
 
 
@@ -23,3 +26,13 @@ fun Context.pxToDp(px: Float) = px / this.resources.displayMetrics.density
  * Converts dp to px
  */
 fun Context.dpToPx(dp: Float) = dp * this.resources.displayMetrics.density
+
+/**
+ * Get color from ContextCompat.getColor(context,color)
+ */
+fun Context.getColorCompat(@ColorRes color: Int) = ContextCompat.getColor(this, color)
+
+/**
+ * Get color from ContextCompat.getColor(context,color)
+ */
+fun Context.getDrawableCompat(@DrawableRes drawable: Int) = ContextCompat.getDrawable(this, drawable)
