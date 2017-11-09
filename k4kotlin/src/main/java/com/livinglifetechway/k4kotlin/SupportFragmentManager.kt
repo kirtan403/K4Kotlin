0e62e674d192f3ec285d512835b1c5fef21d2cd9
Kirtan Thakkar <kirtan403@gmail.com>
2017-11-09 17:57:48 +0530

    Fix transact function marked as private

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/SupportFragmentManager.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/SupportFragmentManager.kt
@@ -6,7 +6,7 @@ import android.support.v4.app.FragmentTransaction
 /**
  * Runs a FragmentTransaction, then calls commit().
  */
-private inline fun FragmentManager.transact(function: FragmentTransaction.() -> Unit) {
+inline fun FragmentManager.transact(function: FragmentTransaction.() -> Unit) {
     beginTransaction().apply {
         function()
     }.commit()
