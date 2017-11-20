16f2d0538fdb28bcc4d56e9932b701d5160e4ab3
Kirtan Thakkar <kirtan403@gmail.com>
2017-11-20 15:44:28 +0530

    Accepts an object of lifecycle owner (instead of just activity) to support activity and fragments to limit retrofit call to owner's lifecycle

--- k4kotlin-retrofit/src/main/java/com/livinglifetechway/k4kotlin_retrofit/Retrofit.kt
+++ k4kotlin-retrofit/src/main/java/com/livinglifetechway/k4kotlin_retrofit/Retrofit.kt
@@ -2,22 +2,22 @@ package com.livinglifetechway.k4kotlin_retrofit
 
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.OnLifecycleEvent
-import android.support.v7.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 
 /**
- * Excepts the activity as the first argument so that,
- * call will continue till the activity is present.
- * Once the activity finishes, the existing call will be cancelled
+ * Excepts the an object of lifecycle owner (activity,fragment etc) as the first argument so that,
+ * call will continue till the lifecycle is active.
+ * Once the lifecycle finishes, the existing call will be cancelled
  * Note: Cancelling an running call will call onFailure method,
  *       so you should handle your cancelled call properly in onFailure
  */
-fun <T> Call<T>.enqueue(activity: AppCompatActivity, callback: Callback<T>) {
+fun <T> Call<T>.enqueue(lifecycleOwner: LifecycleOwner, callback: Callback<T>) {
     // add an observer to activity which will cancel an ongoing call
     // when activity is destroyed
-    activity.lifecycle.addObserver(object : LifecycleObserver {
+    lifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
         @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
         fun cancelCalls() {
             this@enqueue.cancel()
