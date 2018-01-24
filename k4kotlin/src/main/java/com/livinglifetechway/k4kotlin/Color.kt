905a03a708592297b0a3b3f6f07da328a22205ab
Olivier Perez [a570709] <olivier.perez@worldline.com>
2018-01-24 15:45:49 +0000

    Add extension Context.getColorStateList(...)

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Color.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Color.kt
@@ -8,3 +8,8 @@ import android.support.v4.content.ContextCompat
  * Get color from ContextCompat.getColor(context,color)
  */
 fun Context.getColorCompat(@ColorRes color: Int) = ContextCompat.getColor(this, color)
+
+/**
+ * Get color state list from ContextCompat.getColorStateList(context,color)
+ */
+fun Context.getColorStateList(@ColorRes color: Int) = ContextCompat.getColorStateList(this, color)
