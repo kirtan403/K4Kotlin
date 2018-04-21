f2477dd462bb2c9dc9fa407f3ba7c68ce33ad3ca
Kirtan Thakkar <kirtan403@gmail.com>
2018-04-21 23:48:14 +0530

    New toggle method for Collections which removes an element if already present and add if not already present

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Collection.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Collection.kt
@@ -10,6 +10,21 @@ fun <T> MutableCollection<T>.addIfNew(t: T): Boolean = when {
 }
 
 /**
+ * Adds an element to Collection if not already exists
+ * @return true if added else false if removed
+ */
+fun <T> MutableCollection<T>.toggle(t: T): Boolean = when {
+    !contains(t) -> {
+        add(t)
+        true
+    }
+    else -> {
+        remove(t)
+        false
+    }
+}
+
+/**
  * Perform operation if item is there in the collection
  */
 fun <T> Collection<T>.doIfContained(t: T, func: T.() -> Unit): Boolean {
