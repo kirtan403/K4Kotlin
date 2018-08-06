b4d86579927d79a6cd20469454c9e2743196badb
Peter Bryant <peter@rotacloud.com>
2018-08-06 14:49:26 +0100

    Catch possible (although unlikely) exception

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Keyboard.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/Keyboard.kt
@@ -9,9 +9,13 @@ import android.view.inputmethod.InputMethodManager
  */
 fun Activity.hideKeyboard(): Boolean {
     val view = currentFocus
-    view?.let {
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        return inputMethodManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+    try {
+        view?.let {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            return inputMethodManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+    } catch (e: TypeCastException) {
+        return false
     }
     return false
 }
