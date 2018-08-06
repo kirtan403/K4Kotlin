96aeff89e4fe7185df77e2fce63e0ac3e5c1943a
Peter Bryant <peter@rotacloud.com>
2018-08-06 16:38:47 +0100

    Make return type of map and switchmap lambdas nullable

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/LiveData.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/LiveData.kt
@@ -4,7 +4,7 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Transformations
 
 /** Nicer syntax for [Transformations.map] **/
-fun <X, Y> LiveData<X>.map(func: (X) -> Y): LiveData<Y> = Transformations.map(this, func)
+fun <X, Y> LiveData<X>.map(func: (X) -> Y?): LiveData<Y> = Transformations.map(this, func)
 
 /** Nicer syntax for [Transformations.switchMap] **/
-fun <X, Y> LiveData<X>.switchMap(func: (X) -> LiveData<Y>): LiveData<Y> = Transformations.switchMap(this, func)
+fun <X, Y> LiveData<X>.switchMap(func: (X) -> LiveData<Y>?): LiveData<Y> = Transformations.switchMap(this, func)
