bda8c40920662e89a895f6d8923a2083f449c512
Peter Bryant <peter@rotacloud.com>
2018-08-06 13:46:15 +0100

    Document CollectionTest

--- k4kotlin/src/test/java/com/livinglifetechway/k4kotlin/CollectionTest.kt
+++ k4kotlin/src/test/java/com/livinglifetechway/k4kotlin/CollectionTest.kt
@@ -3,8 +3,14 @@ package com.livinglifetechway.k4kotlin
 import org.junit.Assert.*
 import org.junit.Test
 
+/**
+ * Test functions related to collections
+ */
 class CollectionTest {
 
+    /**
+     * Tests that the [addIfNew] method does add an element to a list if it isn't already a member
+     */
     @Test
     fun addIfNew_ShouldAddWhenNew() {
         val list = arrayListOf(20, 14, 34, 983)
@@ -14,6 +20,9 @@ class CollectionTest {
         assertEquals(list, listOf(20, 14, 34, 983, 7))
     }
 
+    /**
+     * Tests that the [addIfNew] method doesn't add an element to a list if it is already a member
+     */
     @Test
     fun addIfNew_ShouldNotAddWhenNotNew() {
         val list = arrayListOf(20, 14, 34, 983)
@@ -23,6 +32,9 @@ class CollectionTest {
         assertEquals(list, arrayListOf(20, 14, 34, 983))
     }
 
+    /**
+     * Tests that the [toggle] method removes an item from a list if it is present
+     */
     @Test
     fun toggle_ShouldRemoveWhenPresent() {
         val list = arrayListOf(20, 14, 34, 983)
@@ -32,6 +44,9 @@ class CollectionTest {
         assertEquals(list, listOf(14, 34, 983))
     }
 
+    /**
+     * Tests that the [toggle] method adds an item from a list if it is not present
+     */
     @Test
     fun toggle_ShouldAddNotPresent() {
         val list = arrayListOf(20, 14, 34, 983)
@@ -41,6 +56,9 @@ class CollectionTest {
         assertEquals(list, listOf(20, 14, 34, 983, 7))
     }
 
+    /**
+     * Tests that [doIfContained] executes its callback when the given list contains the given value
+     */
     @Test
     fun doIfContained_ShouldDoIfContained() {
         val list = arrayListOf(20, 14, 34, 983)
@@ -54,6 +72,9 @@ class CollectionTest {
         assertTrue(done)
     }
 
+    /**
+     * Tests that [doIfContained] does not execute its callback if the given list does not contain the given value
+     */
     @Test
     fun doIfContained_ShouldNotDoIfNotContained() {
         val list = arrayListOf(20, 14, 34, 983)
