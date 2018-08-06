2cc419fcd412766b434905a113b131d03e19492f
Peter Bryant <peter@rotacloud.com>
2018-08-06 16:22:06 +0100

    Add methods for LiveData mapping and switchmapping

---
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/LiveData.kt
@@ -0,0 +1,10 @@
+package com.livinglifetechway.k4kotlin
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Transformations
+
+/** Nicer syntax for [Transformations.map] **/
+fun <X, Y> LiveData<X>.map(func: (X) -> Y): LiveData<Y> = Transformations.map(this, func)
+
+/** Nicer syntax for [Transformations.switchMap] **/
+fun <X, Y> LiveData<X>.switchMap(func: (X) -> LiveData<Y>): LiveData<Y> = Transformations.switchMap(this, func)
---
+++ k4kotlin/src/test/java/com/livinglifetechway/k4kotlin/LiveDataTest.kt
@@ -0,0 +1,70 @@
+package com.livinglifetechway.k4kotlin
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.MutableLiveData
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+
+class LiveDataTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun liveData_Works() {
+        val liveData = MutableLiveData<Int>()
+
+        assertEquals(null, liveData.value)
+
+        liveData.observeForever {}
+
+        liveData.postValue(3)
+
+        assertEquals(3, liveData.value)
+    }
+
+    @Test
+    fun map_Works() {
+        val original = MutableLiveData<Int>()
+
+        assertEquals(null, original.value)
+
+        val map = original.map { it * 3 }
+
+        assertEquals(null, map.value)
+
+        map.observeForever { }
+
+        original.postValue(3)
+
+        assertEquals(3, original.value)
+
+        assertEquals(3 * 3, map.value)
+
+    }
+
+    @Test
+    fun switchMap_Works() {
+        val original = MutableLiveData<Int>()
+
+        assertEquals(null, original.value)
+
+        val switchMap = original.switchMap {
+            MutableLiveData<Int>().apply { value = it * 7 }
+        }
+
+        assertEquals(null, switchMap.value)
+
+        switchMap.observeForever { }
+
+        original.postValue(9)
+
+        assertEquals(9, original.value)
+
+        assertEquals(9 * 7, switchMap.value)
+
+    }
+
+}
