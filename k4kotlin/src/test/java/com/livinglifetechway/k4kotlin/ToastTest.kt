5086ea080c59f843bdaaf54ce52f53fb6feec9f9
Peter Bryant <peter@rotacloud.com>
2018-08-06 14:09:02 +0100

    Document ToastTest

--- k4kotlin/src/test/java/com/livinglifetechway/k4kotlin/ToastTest.kt
+++ k4kotlin/src/test/java/com/livinglifetechway/k4kotlin/ToastTest.kt
@@ -8,30 +8,45 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment.application
 import org.robolectric.Shadows.shadowOf
 
+/**
+ * Test functions related to toasts
+ */
 @RunWith(RobolectricTestRunner::class)
 class ToastTest {
 
     private val shownToasts: List<Toast>
         get() = shadowOf(application).shownToasts
 
+    /**
+     * Display a toast and check it is shown
+     */
     @Test
     fun normalToast() {
         val toast = application.toast("Ping!")
         assertTrue(shownToasts.contains(toast))
     }
 
+    /**
+     * Display a short toast and check it is shown
+     */
     @Test
     fun shortToast() {
         val toast = application.shortToast("Pop!")
         assertTrue(shownToasts.contains(toast))
     }
 
+    /**
+     * Display a long toast and check it is shown
+     */
     @Test
     fun longToast() {
         val toast = application.longToast("Paaaaaarp!")
         assertTrue(shownToasts.contains(toast))
     }
 
+    /**
+     * Display a toast to cancel the previous one and check it is shown
+     */
     @Test
     fun normalToastNow() {
         application.toast("Hello!")
@@ -39,6 +54,9 @@ class ToastTest {
         assertTrue(shownToasts.contains(toast))
     }
 
+    /**
+     * Display a short toast to cancel the previous one and check it is shown
+     */
     @Test
     fun shortToastNow() {
         application.toast("Hello!")
@@ -46,6 +64,9 @@ class ToastTest {
         assertTrue(shownToasts.contains(toast))
     }
 
+    /**
+     * Display a long toast to cancel the previous one and check it is shown
+     */
     @Test
     fun longToastNow() {
         application.toast("Hello!")
