eb9a72c9b4adbbab512e2afa679748472f8512ed
Peter Bryant <peter@rotacloud.com>
2018-09-03 13:25:03 +0100

    Undo previous change - unnecessary

--- k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/EditText.kt
+++ k4kotlin/src/main/java/com/livinglifetechway/k4kotlin/EditText.kt
@@ -8,14 +8,14 @@ import android.widget.EditText
 /**
  * Easier way to get string from edit text and set the string to it
  */
-var <T : EditText> T.value: String
+var EditText.value: String
     get() = text.toString()
     set(value) = setText(value)
 
 /**
  * Sets Transformation to uppercase. Converts all lowercase letters typed to uppercase
  */
-fun <T : EditText> T.setUppercaseTransformation() {
+fun EditText.setUppercaseTransformation() {
     transformationMethod = object : ReplacementTransformationMethod() {
         private val lower = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
         private val upper = charArrayOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
@@ -29,7 +29,7 @@ fun <T : EditText> T.setUppercaseTransformation() {
 /**
  * Sets Transformation to lowercase. Converts all uppercase letters typed to lowercase
  */
-fun <T : EditText> T.setLowercaseTransformation() {
+fun EditText.setLowercaseTransformation() {
     transformationMethod = object : ReplacementTransformationMethod() {
         private val lower = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
         private val upper = charArrayOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
@@ -44,9 +44,9 @@ fun <T : EditText> T.setLowercaseTransformation() {
  * Accepts 3 text watcher methods with default empty implementation.
  * Returns the TextWatcher added to EditText
  */
-fun <T : EditText> T.addTextWatcher(afterTextChanged: (s: Editable?) -> Unit = { _ -> },
-                                    beforeTextChanged: (s: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ -> },
-                                    onTextChanged: (s: CharSequence?, start: Int, before: Int, count: Int) -> Unit = { _, _, _, _ -> }): TextWatcher {
+fun EditText.addTextWatcher(afterTextChanged: (s: Editable?) -> Unit = { _ -> },
+                            beforeTextChanged: (s: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ -> },
+                            onTextChanged: (s: CharSequence?, start: Int, before: Int, count: Int) -> Unit = { _, _, _, _ -> }): TextWatcher {
 
     val textWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {
