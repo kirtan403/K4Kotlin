944ce370e4f8fde451247e426fcd3cb83bc74f96
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-29 19:57:36 +0530

    Add documentation for Collection.kt

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Collection.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Collection.kt
@@ -3,6 +3,9 @@ package com.livinglifetechway.k4kotlin.core
 
 /**
  * Adds an element to Collection if not already exists
+ * @receiver MutableCollection
+ * @param T an element to add if not already present
+ * @return boolean value representing if the element is added or not
  */
 fun <T> MutableCollection<T>.addIfNew(t: T): Boolean = when {
     !contains(t) -> add(t)
@@ -11,6 +14,8 @@ fun <T> MutableCollection<T>.addIfNew(t: T): Boolean = when {
 
 /**
  * Adds an element to Collection if not already exists
+ * @receiver MutableCollection
+ * @param T an element to add if not already present and remove if already present
  * @return true if added else false if removed
  */
 fun <T> MutableCollection<T>.toggle(t: T): Boolean = when {
@@ -26,6 +31,10 @@ fun <T> MutableCollection<T>.toggle(t: T): Boolean = when {
 
 /**
  * Perform operation if item is there in the collection
+ * @receiver Collection
+ * @param T an element to check if exists in the collection
+ * @param func to execute if t is present in the collection
+ * @return true if added else false if removed
  */
 fun <T> Collection<T>.doIfContained(t: T, func: T.() -> Unit): Boolean {
     if (contains(t)) {
