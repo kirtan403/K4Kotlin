db64857edd3065aced99329af01d5d47918d9b21
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-29 20:37:27 +0530

    Adds a show keyboard method to focus on view and forcefully open keyboard

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Keyboard.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Keyboard.kt
@@ -2,10 +2,13 @@ package com.livinglifetechway.k4kotlin.core
 
 import android.app.Activity
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 /**
  * Hides the soft keyboard
+ * @receiver Activity
+ * @return a boolean value if the action was performed or not
  */
 fun Activity.hideKeyboard(): Boolean {
     val view = currentFocus
@@ -14,4 +17,22 @@ fun Activity.hideKeyboard(): Boolean {
         return inputMethodManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
     return false
+}
+
+/**
+ * Opens up the keyboard by focusing on the view
+ * @receiver View
+ */
+fun View.showKeyboard() {
+    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+
+/**
+ * Opens up the keyboard forcefully
+ * @receiver Context
+ */
+fun Context.showKeyboard() {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
 }
