cda18363db58c0eebc91cf2c4c342e477f60caa6
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-29 20:18:44 +0530

    Adds sp to px and px to sp conversions and documentation

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Conversions.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Conversions.kt
@@ -4,10 +4,32 @@ import android.content.Context
 
 /**
  * Converts px to dp
+ * @receiver Context
+ * @param px value in pixels that needs to be converted to dp
+ * @return dp value
  */
 fun Context.pxToDp(px: Float) = px / this.resources.displayMetrics.density
 
 /**
  * Converts dp to px
+ * @receiver Context
+ * @param dp value in dp that needs to be converted to px
+ * @return px value
  */
-fun Context.dpToPx(dp: Float) = dp * this.resources.displayMetrics.density
+fun Context.dpToPx(dp: Float) = (dp * this.resources.displayMetrics.density).toInt()
+
+/**
+ * Converts sp to px
+ * @receiver Context
+ * @param sp value in sp that needs to be converted to px
+ * @return px value
+ */
+fun Context.spToPx(sp: Float) = (sp * this.resources.displayMetrics.scaledDensity).toInt()
+
+/**
+ * Converts px to sp
+ * @receiver Context
+ * @param px value in pixels that needs to be converted to sp
+ * @return sp value
+ */
+fun Context.pxToSp(px: Float) = px / this.resources.displayMetrics.scaledDensity
