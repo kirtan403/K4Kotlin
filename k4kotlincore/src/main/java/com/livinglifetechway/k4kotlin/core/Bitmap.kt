78bb761bf24b0e640e0bf2fad9bfc48b524058f0
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-30 13:42:04 +0530

    convert bitmap to base64 extension

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Bitmap.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Bitmap.kt
@@ -2,7 +2,10 @@ package com.livinglifetechway.k4kotlin.core
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.util.Base64
 import android.view.View
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 
 /**
@@ -16,4 +19,30 @@ fun View.getBitmap(bitmapConfig: Bitmap.Config = Bitmap.Config.ARGB_8888): Bitma
     draw(canvas)
     canvas.save()
     return bmp
+}
+
+/**
+ * Converts bitmap to bas64 string
+ * @return base64 representation of the bitmap
+ */
+fun Bitmap.toBase64(): String {
+    var result = ""
+    val baos = ByteArrayOutputStream()
+    try {
+        compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        baos.flush()
+        baos.close()
+        val bitmapBytes = baos.toByteArray()
+        result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT)
+    } catch (e: IOException) {
+        e.printStackTrace()
+    } finally {
+        try {
+            baos.flush()
+            baos.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    return result
 }
