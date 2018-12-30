9c7a77621628d66673f225b606d1f87154b4db8c
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-30 10:34:35 +0530

    Update documentation for Network.kt

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Network.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/Network.kt
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager
 /**
  * Checks for network availability
  * NOTE: Don't forget to add android.permission.ACCESS_NETWORK_STATE permission to manifest
+ * @return a boolean representing if network is available or not
  */
 fun Context.isNetworkAvailable(): Boolean {
     val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
