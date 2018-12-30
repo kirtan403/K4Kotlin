27ab077f1ff2cc6ae3917bc681a8cd201053378f
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-30 12:04:43 +0530

    Update documentation for String.kt

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/String.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/String.kt
@@ -2,12 +2,14 @@ package com.livinglifetechway.k4kotlin.core
 
 /**
  * Checks if a string is a valid email
+ * @return a boolean representing true if email is valid else false
  */
 fun String.isEmail() = android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
 /**
  * Checks if String is Number.
  * Checks against regex `^[0-9]+$`
+ * @return a boolean representing true if all the characters are numeric else false
  */
 fun String.isNumeric(): Boolean {
     val p = "^[0-9]+$".toRegex()
