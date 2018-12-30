0f5afe06cbdf6caed00e8c8c826019c10393854e
Kirtan Thakkar <kirtan403@gmail.com>
2018-12-30 11:54:34 +0530

    Common for orZero for number and orDefault method for all the numbers

--- k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/NullSafety.kt
+++ k4kotlincore/src/main/java/com/livinglifetechway/k4kotlin/core/NullSafety.kt
@@ -1,33 +1,26 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package com.livinglifetechway.k4kotlin.core
 
 /**
  * Returns Zero (0) if it is null
  */
-fun Number?.orZero(): Number = this ?: 0
-
-/**
- * Returns Zero (0) if it is null
- */
-fun Int?.orZero(): Int = this ?: 0
-
-/**
- * Returns Zero (0) if it is null
- */
-fun Long?.orZero(): Long = this ?: 0
-
-/**
- * Returns Zero (0) if it is null
- */
-fun Float?.orZero(): Float = this ?: 0.0f
-
-/**
- * Returns Zero (0) if it is null
- */
-fun Double?.orZero(): Double = this ?: 0.0
+@Suppress("UNCHECKED_CAST")
+fun <T : Number> T?.orZero(): T = this ?: (0 as T)
 
 /**
  * Returns false if it is null
  */
 fun Boolean?.orFalse(): Boolean = this ?: false
+
+/**
+ * Returns true if it is null
+ * Warning: Please use this with caution. Boolean's default value is false
+ */
+fun Boolean?.orTrue(): Boolean = this ?: true
+
+/**
+ * Returns the default value specified in the argument
+ * @param value default value
+ * @return The number itself if not null or the default value casted to receiver type
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T : Number, J : Number> T?.orDefault(value: J): T = this ?: (value as T)
