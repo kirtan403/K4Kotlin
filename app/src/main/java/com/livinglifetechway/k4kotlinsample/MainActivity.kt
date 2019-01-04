67cebbfb7d8b9b0b01035ac3a0b31e412d8c8425
Kirtan Thakkar <kirtan403@gmail.com>
2019-01-05 01:29:47 +0530

    Add logs sample

--- app/src/main/java/com/livinglifetechway/k4kotlinsample/MainActivity.kt
+++ app/src/main/java/com/livinglifetechway/k4kotlinsample/MainActivity.kt
@@ -3,11 +3,14 @@ package com.livinglifetechway.k4kotlinsample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.livinglifetechway.k4kotlin.core.debug
+import com.livinglifetechway.k4kotlin.core.err
 import com.livinglifetechway.k4kotlin.core.fadeIn
 import com.livinglifetechway.k4kotlin.core.fadeOut
 import com.livinglifetechway.k4kotlin.core.hide
 import com.livinglifetechway.k4kotlin.core.hideKeyboard
 import com.livinglifetechway.k4kotlin.core.hideViews
+import com.livinglifetechway.k4kotlin.core.info
 import com.livinglifetechway.k4kotlin.core.invisible
 import com.livinglifetechway.k4kotlin.core.onClick
 import com.livinglifetechway.k4kotlin.core.onItemSelected
@@ -19,6 +22,9 @@ import com.livinglifetechway.k4kotlin.core.showViews
 import com.livinglifetechway.k4kotlin.core.startActivity
 import com.livinglifetechway.k4kotlin.core.toast
 import com.livinglifetechway.k4kotlin.core.toggleVisibility
+import com.livinglifetechway.k4kotlin.core.verbose
+import com.livinglifetechway.k4kotlin.core.warning
+import com.livinglifetechway.k4kotlin.core.wtf
 import com.livinglifetechway.k4kotlinsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -46,7 +52,15 @@ class MainActivity : AppCompatActivity() {
         // set spinner - example
         data class Country(var name: String, var code: String)
 
-        val countries: ArrayList<Country> = ArrayList<Country>()
+        // some logs are here
+        debug("This is a debug log!")
+        info("This is info log")
+        err("err log it is...")
+        verbose("What's this, verbose? ")
+        warning("I am warning you!!")
+        wtf("WTF!")
+
+        val countries: ArrayList<Country> = ArrayList()
         countries.add(Country("India", "IND"))
         countries.add(Country("United States of America", "USA"))
         mBinding.spinner.setItems(countries) { it.name + " (" + it.code + ")" }
