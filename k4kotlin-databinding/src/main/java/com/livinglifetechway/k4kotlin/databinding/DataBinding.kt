eacde65ff96bed101d602fc96c6b0248c0914ad0
Kirtan Thakkar <kirtan403@gmail.com>
2019-01-05 00:47:29 +0530

    Remove databinding binding component from setBindingView method

--- k4kotlin-databinding/src/main/java/com/livinglifetechway/k4kotlin/databinding/DataBinding.kt
+++ k4kotlin-databinding/src/main/java/com/livinglifetechway/k4kotlin/databinding/DataBinding.kt
@@ -5,20 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 /**
  * Sets the activity's content view with the data binding
  * @param layoutRes Layout resource to use as the activity's content
- * @param bindingComponent optional
  * @return The ViewDataBinding instance of the specified type
  */
 fun <T : ViewDataBinding> Activity.setBindingView(
-        @LayoutRes layoutRes: Int,
-        bindingComponent: DataBindingComponent? = null
-) = DataBindingUtil.setContentView<T>(this, layoutRes, bindingComponent)
+        @LayoutRes layoutRes: Int
+): T = DataBindingUtil.setContentView(this, layoutRes)
 
 /**
  * Binds a view to specified view data binding
